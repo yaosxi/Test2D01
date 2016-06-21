bc832ab90402dafff8aa969aa3377d01644a54ae
yaosxi <yaosxi@gmail.com>
2016-06-21 15:07:01 +0800

    1

---
+++ Assets/Editor/AutoBuild.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+public class AutoBuild  {
+	public static void BuildAndroid()
+	{
+		string[] levels = { "Assets/Scenes/GameScene.unity"};
+		BuildPipeline.BuildPlayer(levels, "test2d01.apk", BuildTarget.Android, BuildOptions.None);
+	}
+}
---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {    
+		//改变字体大小  
+		GUI.skin.label.fontSize = 120;   
+		//定位显示(左边距x, 上边距y, 宽, 高)  
+		GUI.Label (new Rect (10, 50, 900, 120), "Hello World!");  
+	}  
+}
